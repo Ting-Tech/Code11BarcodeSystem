f7e874e52e8561555fcae830deb42101fa990b4d
AllenTing <67228710+Ting-Tech@users.noreply.github.com>
2023-12-06 00:53:31 +0800

    Finish C K correct code calibration

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 
 struct EncodeElement
 {
-    char *characture;
+    int characture;
     int value[5];
 };
 
@@ -68,21 +68,21 @@ bool toBinary(int array[], int len)
     return true;
 }
 
-char *searchTable(int start, int array[])
+int searchTable(int start, int array[])
 {
     const struct EncodeElement table[] = {
-        {"1", {1, 0, 0, 0, 1}},
-        {"2", {0, 1, 0, 0, 1}},
-        {"3", {1, 1, 0, 0, 0}},
-        {"4", {0, 0, 1, 0, 1}},
-        {"5", {1, 0, 1, 0, 0}},
-        {"6", {0, 1, 1, 0, 0}},
-        {"7", {0, 0, 0, 1, 1}},
-        {"8", {1, 0, 0, 1, 0}},
-        {"9", {1, 0, 0, 0, 0}},
-        {"0", {0, 0, 0, 0, 1}},
-        {"-", {0, 0, 1, 0, 0}},
-        {"Start/Stop", {0, 0, 1, 1, 0}}};
+        {1, {1, 0, 0, 0, 1}},
+        {2, {0, 1, 0, 0, 1}},
+        {3, {1, 1, 0, 0, 0}},
+        {4, {0, 0, 1, 0, 1}},
+        {5, {1, 0, 1, 0, 0}},
+        {6, {0, 1, 1, 0, 0}},
+        {7, {0, 0, 0, 1, 1}},
+        {8, {1, 0, 0, 1, 0}},
+        {9, {1, 0, 0, 0, 0}},
+        {0, {0, 0, 0, 0, 1}},
+        {10, {0, 0, 1, 0, 0}},  // -
+        {11, {0, 0, 1, 1, 0}}}; // Start/Stop
 
     for (size_t i = 0; i < 12; i++)
     {
@@ -101,14 +101,14 @@ char *searchTable(int start, int array[])
         }
     }
 
-    return NULL;
+    return -1;
 }
 
 void fixCodeDirection(int array[], int size)
 {
-    char *firstEle = searchTable(0, array);
+    int firstEle = searchTable(0, array);
 
-    if (firstEle != "Start/Stop")
+    if (firstEle != 11)
     {
         int start = 0;
         int end = size - 1;
@@ -126,28 +126,105 @@ void fixCodeDirection(int array[], int size)
     }
 }
 
-char **decoding(int array[], int len)
+int decoding(int array[], int len, int resultNoCorrectCode[])
 {
-    char **result = (char **)malloc((len + 1) / 6 * sizeof(char *));
-    for (int x = 0; x < (len + 1) / 6; x++)
-    {
-        char *element = searchTable(5 * x + x, array);
+    int strCount = ((len + 1) / 6) - 2;
+    int *result = (int *)malloc(strCount * sizeof(int));
 
-        if (element != NULL)
+    for (int x = 0; x < strCount; x++)
+    {
+        int element = searchTable(5 * (x + 1) + x + 1, array);
+
+        if (element != -1)
         {
             result[x] = element;
         }
     }
-    return result;
+
+    for (int x = 0; x < strCount - 2; x++)
+    {
+        int element = searchTable(5 * (x + 1) + x + 1, array);
+
+        if (element != -1)
+        {
+            resultNoCorrectCode[x] = element;
+        }
+    }
+
+    // for (size_t i = 0; i < strCount; i++)
+    // {
+    //     printf("Result:%d\n", result[i]);
+    // }
+
+    // printf("\n");
+
+    // for (size_t i = 0; i < strCount - 2; i++)
+    // {
+    //     printf("ResultB:%d\n", resultNoCorrectCode[i]);
+    // }
+
+    // printf("\n");
+
+    int readC = result[strCount - 2];
+    int readK = result[strCount - 1];
+    // printf("Read C:%d\n", readC);
+    // printf("Read K:%d\n", readK);
+
+    int sumC = 0;
+    int countWeightC = 1;
+    for (size_t i = 0; i < strCount - 2; i++)
+    {
+        sumC += resultNoCorrectCode[i] * countWeightC;
+        if (countWeightC == 10)
+        {
+            countWeightC = 1;
+        }
+        else
+        {
+            countWeightC++;
+        }
+    }
+    int c = sumC % 11;
+    if (c != readC)
+    {
+        return -1;
+    }
+
+    // printf("%d\n", c);
+
+    int sumK = c;
+    int countWeightK = 2;
+    for (size_t i = 0; i < strCount - 2; i++)
+    {
+        sumK += resultNoCorrectCode[i] * countWeightK;
+        if (countWeightK == 9)
+        {
+            countWeightK = 1;
+        }
+        else
+        {
+            countWeightK++;
+        }
+    }
+    int k = sumK % 11;
+    if (k != readK)
+    {
+        return -2;
+    }
+    // printf("%d\n", k);
+
+    return 0;
 }
 
 int main()
 {
     int *code;
+    int *barcodeResult;
     int codeLen = 0;
 
     scanf("%d", &codeLen);
     code = (int *)malloc(codeLen * sizeof(int));
+    barcodeResult = (int *)malloc((((codeLen + 1) / 6) - 4) * sizeof(int));
 
     for (size_t i = 0; i < codeLen; i++)
     {
@@ -160,25 +237,44 @@ int main()
         return -1;
     }
 
-    for (size_t i = 0; i < codeLen; i++)
-    {
-        printf("%d ", code[i]);
-    }
+    // for (size_t i = 0; i < codeLen; i++)
+    // {
+    //     printf("%d ", code[i]);
+    // }
 
-    printf("\n");
+    // printf("\n");
 
     fixCodeDirection(code, codeLen);
 
-    for (size_t i = 0; i < codeLen; i++)
-    {
-        printf("%d ", code[i]);
-    }
+    // for (size_t i = 0; i < codeLen; i++)
+    // {
+    //     printf("%d ", code[i]);
+    // }
 
-    printf("\n");
+    // printf("\n");
 
-    char **decodingResult = decoding(code, codeLen);
-    for (size_t i = 0; i < (codeLen + 1) / 6; i++)
+    int indexResult = decoding(code, codeLen, barcodeResult);
+    if (indexResult == 0)
     {
-        printf("Result:%s\n", decodingResult[i]);
+        for (size_t a = 0; a < (((codeLen + 1) / 6) - 4); a++)
+        {
+            if (barcodeResult[a] == 10)
+            {
+                printf("-");
+            }
+            else
+            {
+                printf("%d", barcodeResult[a]);
+            }
+        }
     }
+    else if (indexResult == -1)
+    {
+        printf("bad C");
+    }
+    else if (indexResult == -2)
+    {
+        printf("bad K");
+    }
+    return 0;
 }
